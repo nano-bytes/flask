b1bec7b6aa12935965c0f3e9d862cd1deb2671b2
Daniel Córdova A <danesc87@gmail.com>
2019-10-19 16:41:39 -0500

    Add get and post methods

--- simple-todo/app/views/todo.py
+++ simple-todo/app/views/todo.py
@@ -5,23 +5,52 @@
 """
 
 from app import app
+import json
+from flask import abort
+from flask import request
+from flask import jsonify
+from ..utils import json_utils
+
+todo_list = ['{"id": 1, "title": "first", "description": "description"}']
 
 
 @app.route("/", methods=['GET'])
 def get_all_todos():
-    return "TODO's"
+    # Return Json List
+    return jsonify(
+        [json.loads(each_todo) for each_todo in todo_list]
+    )
 
 
-@app.route("/"+'<int:todo_id>', methods=['GET'])
+@app.route("/" + '<string:todo_id>', methods=['GET'])
 def get_todo(todo_id):
-    return "TODO"
+    selected_todo = None
+    for element in todo_list:
+        if str(json.loads(element)['id']) == todo_id:
+            selected_todo = element
+    # If Json is None we return a 404 Error code
+    if selected_todo is None:
+        return abort(404)
+    return selected_todo
 
 
 @app.route("/", methods=['POST'])
 def post_todo():
-    return "POSTED"
+    # Check if request is a valid JSON
+    json_utils.is_not_json_request(request)
+    id_list = []
+    # Iterate To-Do List, take only Id's and add it to a new list
+    for element in todo_list:
+        id_list.append(json.loads(element)['id'])
+    # Order List of id's
+    id_list.sort()
+    # Add id to Json
+    request.json['id'] = id_list[-1] + 1
+    # Add json to To-Do list
+    todo_list.append(str(json.dumps(request.json)))
+    return json.dumps(request.json)
 
 
-@app.route("/"+'<int:todo_id>', methods=['PUT'])
+@app.route("/" + '<string:todo_id>', methods=['PUT'])
 def put_todo(todo_id):
     return "UPDATED"
