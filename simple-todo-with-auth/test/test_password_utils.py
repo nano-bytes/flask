0c7739fac744e0dc58be0b2a21373763a43f73b7
Daniel Córdova A <danesc87@gmail.com>
2020-01-31 20:40:43 -0500

    Fixes on unit tests

--- simple-todo-with-auth/test/test_password_utils.py
+++ simple-todo-with-auth/test/test_password_utils.py
@@ -5,12 +5,12 @@ from app.utils.password_utils import get_hashed_password_with_sha512
 
 class GetSha512Test(unittest.TestCase):
 
-    def test_if_can_sha512(self):
+    def test_work_if_is_sha512(self):
         expected = hashlib.sha512("Hello".encode('utf-8')).hexdigest()
         actual = get_hashed_password_with_sha512("Hello")
         self.assertEqual(expected, actual)
 
-    def fail_if_not_sah512(self):
+    def test_fail_if_not_sah512(self):
         expected = hashlib.sha1("Hello".encode('utf-8')).hexdigest()
         actual = get_hashed_password_with_sha512("Hello")
         self.assertNotEqual(expected, actual)
